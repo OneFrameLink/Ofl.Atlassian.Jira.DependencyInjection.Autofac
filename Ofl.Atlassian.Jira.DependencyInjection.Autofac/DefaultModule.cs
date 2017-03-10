c36fca891a01800327c745c8b53c78785781706a
casperOne <casperOne@caspershouse.com>
2017-03-10 09:31:28 -0500

    Initial build.

---
+++ Ofl.Atlassian.Jira.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autofac;
+using Ofl.Atlassian.Jira.V2;
+
+namespace Ofl.Atlassian.Jira.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        #region Overrides of Module
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<JiraClient>().As<IJiraClient>();
+            builder.RegisterType<BasicAuthenticationCredentialProvider>().As<ICredentialProvider>();
+        }
+
+        #endregion
+    }
+}
