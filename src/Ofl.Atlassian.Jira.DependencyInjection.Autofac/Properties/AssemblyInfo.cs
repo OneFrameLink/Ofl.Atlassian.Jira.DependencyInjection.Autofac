f119af4650526711a66a8ca9025265c88f8d5517
casperOne <casperOne@caspershouse.com>
2017-05-23 16:37:43 -0400

    Fixed build.

--- src/Ofl.Atlassian.Jira.DependencyInjection.Autofac/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Ofl.Atlassian.Jira.DependencyInjection.Autofac")]
-[assembly: AssemblyDescription("Ofl.Atlassian.Jira.Contract implementation registrations in Autofac.")]
-[assembly: AssemblyProduct("Ofl.Atlassian.Jira.DependencyInjection.Autofac")]
