2391b3464c7f8009c28cd09834b3bc4a7fc97317
casperOne <casperOne@caspershouse.com>
2017-05-23 16:32:19 -0400

    Added test project.

---
+++ test/Ofl.Atlassian.Jira.DependencyInjection.Autofac.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Ofl.Atlassian.Jira.DependencyInjection.Autofac.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
